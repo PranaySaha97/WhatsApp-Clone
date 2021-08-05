3194f2b421162ea3579cbe6f6a79fdc968b739ee
pranaysaha97 <saha.pranay97@gmail.com>
2021-08-05 13:42:49 +0530

    Updated readme and firebase config

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# WhatsApp-Clone
+# WhatsApp Clone
+
+This is a clone of whatsApp  messaging platform.
+<br/>
+### Tech Stack:
+#### UI: React
+#### Backend: Firebase
+#### Deployed: Firebase.
+
+<br/>
+
+Special thanks to <a href="https://www.youtube.com/channel/UCqrILQNl5Ed9Dz6CGMyvMTQ"><b>Clever Programmer</b></a> for making this <a href="https://www.youtube.com/watch?v=pUxrDcITyjg&list=PL-J2q3Ga50oMQa1JdSJxYoZELwOJAXExP&index=16">tutorial</a>.
+
+<br/>
+
+You can access the hosted site at: https://whatsapp-clone-419a9.web.app/
--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 //GET Below Settings from Firebase > Project Overview > Settings > General > Your apps > Firebase SDK snippet > Config
 const firebaseConfig = {
-	apiKey: "AIzaSyAOsjBcga79GVtrzIY0wyzGTX4TSEUmbMk",
-	authDomain: "whatsapp-clone-419a9.firebaseapp.com",
-	projectId: "whatsapp-clone-419a9",
-	storageBucket: "whatsapp-clone-419a9.appspot.com",
-	messagingSenderId: "1015311062338",
-	appId: "1:1015311062338:web:6a96d38bc2cdbfcadd64db",
-	measurementId: "G-PVJXXSTKMT"
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
+	measurementId: ""
       };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
